9d2a70f0de85c9f5b05af7b385e69080f9e25301
adamalcantara <alcantaradevelopment@gmail.com>
2024-08-25 15:33:41 -0400

    Image added to file tree, pubspec.yaml updated, image showing in app

--- lib/main.dart
+++ lib/main.dart
@@ -11,41 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return the app
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         // Safe area which ignores bezels, notches, dynamic islands, etc
         body: SafeArea(
           // Column which can hold multiple things
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                color: Colors.blue,
-                child: const Text('Container 2'),
-              ),
-               Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.green,
-                  )
-                ],
-              ),
-              Container(
-                color: Colors.red,
-                child: const Text('Container 3'),
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/adam.jpg'),
               ),
             ],
-          ),
+          )
         ),
       ),
     );
