2fd6967dd862b96939815dbadcaf106e3c8d9410
adamalcantara <alcantaradevelopment@gmail.com>
2024-08-25 10:26:23 -0400

    Changed code to stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp()
+  );
+}
+
+//stateless widget
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return the app
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Container(),
+      ),
+    );
+  }
 }
