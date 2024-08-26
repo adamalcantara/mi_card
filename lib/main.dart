992cf3babc805a3b347416843c17365f18fce535
adamalcantara <alcantaradevelopment@gmail.com>
2024-08-25 22:34:00 -0400

    Added divider

--- lib/main.dart
+++ lib/main.dart
@@ -18,74 +18,82 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
             // Column which can hold multiple things
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/adam.jpg'),
-              ),
-              Text(
-                'Adam Alcantara',
-                style: TextStyle(
-                  fontFamily: 'NewAmsterdam',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text('Software Engineer',
-                  style: TextStyle(
-                    fontFamily: 'SourceSans',
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    letterSpacing: 1.5,
-                  )),
-              Card(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/adam.jpg'),
+            ),
+            Text(
+              'Adam Alcantara',
+              style: TextStyle(
+                fontFamily: 'NewAmsterdam',
+                fontSize: 40.0,
                 color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text('Software Engineer',
+                style: TextStyle(
+                  fontFamily: 'SourceSans',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  letterSpacing: 1.5,
+                )),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.white,
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '(313) 867-5309',
+                    style: TextStyle(
                       color: Colors.teal,
-                    ),
-                    title: Text('(313) 867-5309',
-                        style: TextStyle(
-                          color: Colors.teal,
-                          fontFamily: 'SourceSans',
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      fontFamily: 'SourceSans',
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
               ),
-              Card(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(
-                      vertical: 10.0, horizontal: 25.0),
-                  child: Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.email,
-                        color: Colors.teal,
-                      ),
-                      title: Text('alcantaradevelopment@gmail.com',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            color: Colors.teal,
-                            fontFamily: 'SourceSans',
-                            fontWeight: FontWeight.bold,
-                          ),
-                      ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'alcantaradevelopment@gmail.com',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.teal,
+                      fontFamily: 'SourceSans',
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
+                ),
               ),
-            ],
-          )),
+            ),
+          ],
+        )),
       ),
     );
   }
